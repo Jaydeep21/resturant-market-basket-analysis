a33c2b3d975fb705cb873c4f7ac06df4f7366fed
Jaydeep <jaydeepdharamsey21@gmail.com>
2022-11-25 22:01:05 -0500

    updated with dataset

--- userinterface.py
+++ userinterface.py
@@ -1,36 +1,31 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-from apyori import apriori
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
-
+import os
   
-client = MongoClient("localhost", 27017)
+client = MongoClient(os.environ['MONGO_URL'])
 db = client["adtProject"]
 new_df = pd.DataFrame(db["rules"].find({}))
 words = st.sidebar.selectbox("No.of Words", range(10,1000,10))
 st.title('Market Basket Analysis')
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.sidebar.header("Select Item")
-Input = st.sidebar.selectbox('Object Variables', list(set(new_df["Bought Item"])))
-print(Input)
-sample = new_df[new_df['Bought Item'] == Input]
+st.header("Select Item")
+input = st.multiselect(
+    'What are your favorite dishes',
+    list(set(new_df["Bought Item"])))
+sample = new_df 
+if input:
+    sample = new_df[new_df['Bought Item'].isin(input)]
 lis1 = []
 for i in sample["Expected To Be Bought"]:
     lis1.append(i)
 space = " "
 output = space.join(lis1)
 output_final = output.replace("nan", "")
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 32px;">Recommended Items for above selected Item</p>'
-st.markdown(new_title, unsafe_allow_html=True)
-
-#st.markdown("Recommended Items for above selected Item")
-st.write(output_final)
-st.write("Word Cloud Plot")
+st.header("Word Cloud Plot")
 wordcloud = WordCloud(background_color="white", max_words=words).generate(output_final)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
